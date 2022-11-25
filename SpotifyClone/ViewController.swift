10903e52a1a30b8211d0341f1708d743e88b2061
Sultan Almukhan <almukhansultan@gmail.com>
2022-11-25 15:09:13 +0600

    create project

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Sultan Almukhan on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
